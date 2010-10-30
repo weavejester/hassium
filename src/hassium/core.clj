54f8f7566e9216c96cab64f586603fc3c7039194
weavejester <jreeves@weavejester.com>
2010-10-30 23:13:09 +0100

    Added in functions and macros to handle connecting to MongoDB

--- src/hassium/core.clj
+++ src/hassium/core.clj
@@ -1,16 +1,39 @@
 (ns hassium.core
   (:import [com.mongodb Mongo BasicDBObject]))
 
-(def m (Mongo.))
+(defn connect
+  "Connect to a MongoDB server."
+  [connection]
+  (Mongo. (:host connection "127.0.0.1")
+          (:port connection 27017)))
 
-(def db (.getDB m "mydb"))
+(declare *connection*)
 
-(def coll (.getCollection db "testCollection"))
+(defmacro with-connection
+  "Evaluates its body in the context of the supplied MongoDB connection.
+  The connection address is specified as a map, with keys for :host and
+  :port."
+  [address & body]
+  `(binding [*connection* (connect ~address)]
+     (try
+       ~@body
+       (finally (.close *connection*)))))
+
+(defn database
+  "Returns a MongoDB database object."
+  [name]
+  (.getDB *connection* name))
+
+(defn collection
+  "Returns a MongoDB collection object."
+  [db name]
+  (.getCollection db name))
+
+(with-connection {}
+  (prn (-> (database "mydb")
+           (collection "testCollection")
+           (.findOne))))
 
 (def document
   (doto (BasicDBObject.)
     (.put "hello" "world")))
-
-(.save coll document)
-
-(prn (.findOne coll))
