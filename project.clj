82d38af250bc6eedfb99922b5094f50fb1d2dfca
weavejester <jreeves@weavejester.com>
2010-10-30 22:09:17 +0100

    First example code that does something with MongoDB

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.mongodb/mongo-java-driver "2.1"]]
+                 [org.mongodb/mongo-java-driver "2.3"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
                      [swank-clojure "1.2.1"]])
--- src/hassium/core.clj
+++ src/hassium/core.clj
@@ -1,1 +1,16 @@
-(ns hassium.core)
+(ns hassium.core
+  (:import [com.mongodb Mongo BasicDBObject]))
+
+(def m (Mongo.))
+
+(def db (.getDB m "mydb"))
+
+(def coll (.getCollection db "testCollection"))
+
+(def document
+  (doto (BasicDBObject.)
+    (.put "hello" "world")))
+
+(.save coll document)
+
+(prn (.findOne coll))
