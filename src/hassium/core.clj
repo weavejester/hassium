cfbfcf3e7df08e8f01dbe2f4c003bedd84195a9a
weavejester <jreeves@weavejester.com>
2010-10-31 00:39:58 +0100

    Added as-mongo and insert functions

--- src/hassium/core.clj
+++ src/hassium/core.clj
@@ -1,5 +1,11 @@
 (ns hassium.core
-  (:import [com.mongodb Mongo BasicDBObject]))
+  (:import [com.mongodb Mongo BasicDBObject]
+           [clojure.lang Keyword Symbol]
+           [java.util Map List]))
+
+(declare *connection*)
+
+(declare *database*)
 
 (defn connect
   "Connect to a MongoDB server."
@@ -7,7 +13,10 @@
   (Mongo. (:host connection "127.0.0.1")
           (:port connection 27017)))
 
-(declare *connection*)
+(defn database
+  "Returns a MongoDB database object."
+  [name]
+  (.getDB *connection* name))
 
 (defmacro with-connection
   "Evaluates its body in the context of the supplied MongoDB connection.
@@ -15,24 +24,47 @@
   :port."
   [address & body]
   `(binding [*connection* (connect ~address)]
-     (try
-       ~@body
-       (finally (.close *connection*)))))
-
-(defn database
-  "Returns a MongoDB database object."
-  [name]
-  (.getDB *connection* name))
+     (binding [*database* (database (:database ~address))]
+       (try ~@body
+            (finally (.close *connection*))))))
 
 (defn collection
   "Returns a MongoDB collection object."
-  [db name]
-  (.getCollection db name))
+  ([name]    (collection *database* name))
+  ([db name] (.getCollection db name)))
 
-(with-connection {}
-  (prn (-> (database "mydb")
-           (collection "testCollection")
-           (.findOne))))
+(defprotocol AsMongo
+  (as-mongo [x] "Turn x into a com.mongodb.DBObject"))
+
+(extend-protocol AsMongo
+  Map
+  (as-mongo [m]
+    (let [dbo (BasicDBObject.)]
+      (doseq [[k v] m]
+        (.put dbo (name k) (as-mongo v)))
+      dbo))
+  List
+  (as-mongo [coll]
+    (map as-mongo coll))
+  Keyword
+  (as-mongo [kw] (name kw))
+  Symbol
+  (as-mongo [s] (name s))
+  String
+  (as-mongo [s] s)
+  nil
+  (as-mongo [_] nil))
+
+(defn insert
+  "Insert the supplied document maps into the MongoDB collection."
+  [collection & documents]
+  (doseq [doc documents]
+    (.save collection (as-mongo document))))
+
+(with-connection {:database "mydb"}
+  (let [coll (collection "testCollection")]
+    (insert coll {:foo "bar"})
+    (prn (.findOne coll))))
 
 (def document
   (doto (BasicDBObject.)
