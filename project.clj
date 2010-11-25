0e0131fb15df0c3c5fa7016372a48bfb5dbfcbaf
weavejester <jreeves@weavejester.com>
2010-11-25 22:42:14 +0000

    Released 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hassium "0.1.0"
+(defproject hassium "0.2.0"
   :description "MongoDB client library"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.mongodb/mongo-java-driver "2.3"]])
